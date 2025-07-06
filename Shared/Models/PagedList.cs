c1994b087d6bb3f371f6721e30a271eb2d527118
Steven Villarreal <villarst@mail.gvsu.edu>
2025-07-06 18:02:33 -0400

    Added PagedList file for pagination of results.

---
+++ Shared/Models/PagedList.cs
@@ -0,0 +1,19 @@
+namespace Shared.Models;
+
+public class PagedList<T>
+{
+    public List<T> Data { get; set; } = new List<T>();
+    public int TotalCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+}
+
+public class PagedRequest
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 12; // Good for 3x4 grid
+    public string SearchTerm { get; set; } = string.Empty;
+}
